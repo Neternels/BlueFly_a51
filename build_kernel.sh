8a844abda1be863f16b0b53763a49b024af00111
Hendra Manudinata <hendra32os@gmail.com>
2021-04-21 00:48:51 +0000

    build_kernel.sh: modify for build

--- build_kernel.sh
+++ build_kernel.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
+# Compiler path
+CLANG_PATH=${HOME}/AndroidProject/clang-4639204/bin/clang
+GCC_PATH=${HOME}/AndroidProject/gcc49-los18/bin/aarch64-linux-android-
+CLANG_TRIPLE_PATH=aarch64-linux-gnu-
 
-export PLATFORM_VERSION=11
-export ANDROID_MAJOR_VERSION=r
-export ARCH=arm64
+# Start
+make clean && make mrproper
 
-make ARCH=arm64 exynos9610-a51xx_defconfig
-make ARCH=arm64 -j64
+make bluefly_defconfig O=out
+make O=out CC=${CLANG_PATH} CROSS_COMPILE=${GCC_PATH} CLANG_TRIPLE=${CLANG_TRIPLE_PATH} -j64
